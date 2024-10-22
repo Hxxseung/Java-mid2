1859d68f3ee3347e2d7c88ebc2d57933283cda23
Hxxseung <soaorsith2@naver.com>
2024-10-22 17:08:29 +0900

    Set End

---
+++ src/collection/set/test/UniqueNamesTest3.java
@@ -0,0 +1,12 @@
+package collection.set.test;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UniqueNamesTest3 {
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new LinkedHashSet<>();
+    }
+}
